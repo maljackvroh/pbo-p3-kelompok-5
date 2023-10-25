9638be68e17d15ea6f6c755dc43e498733a7c35c
notforsilent <notforsilent@gmail.com>
2023-10-25 23:54:38 +0800

    berkas baru:     jobsheet/akmal_dzaki/js8.java 	berkas baru:     jobsheet/akmal_dzaki/js9.java

---
+++ jobsheet/akmal_dzaki/js8.java
@@ -0,0 +1,33 @@
+package jobsheet.akmal_dzaki;
+
+import java.util.Scanner;
+
+class car {
+    int hitungKecepatan;
+    int jarak;
+    int waktu;
+
+    // Rumus Kecepatan = jarak / Waktu
+    car(int jarak, int waktu){
+        hitungKecepatan = jarak/waktu;
+    }
+}
+
+public class js8 {
+    public static void main(String[] args) {
+        // Deklarasi objek scanner
+        Scanner obj = new Scanner(System.in);
+
+        // input nilai jarak dan waktu
+        System.out.println("\nMasukan jarak: ");
+        int jarak = obj.nextInt();
+        System.out.println("Masukan Waktu: ");
+        int waktu = obj.nextInt();
+
+        // deklarasi objek
+        car air = new car(jarak, waktu);
+
+        // tampilkan nilai/data
+        System.out.println("Hasil Kecepatan air berdasarkan rumus jarak/waktu: "+air.hitungKecepatan);
+    }
+}
