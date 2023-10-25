fefc31614352b84ded2f20111b9c316e389dfcd6
MugniAlRasyid <148302106+MugniAlRasyid@users.noreply.github.com>
2023-10-25 20:52:01 +0800

    Add files via upload

---
+++ jobsheet/mugni_al_rasyid/P3nomor1.java
@@ -0,0 +1,14 @@
+package percobaan3;
+
+public class Mahasiswa {
+    int x = 7;
+    
+    public Mahasiswa(){
+        
+    }
+    public static void main(String[] args) {
+      Mahasiswa mhs = new Mahasiswa();
+        System.out.println(mhs.x);
+    }
+    
+}
