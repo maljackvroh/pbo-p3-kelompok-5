d2d560f98c9f2ceea06b4d6cbcf5badce49b651d
notforsilent <notforsilent@gmail.com>
2023-10-25 19:40:56 +0800

    terubah:         jobsheet/akmal_dzaki/js4.java

--- jobsheet/akmal_dzaki/js4.java
+++ jobsheet/akmal_dzaki/js4.java
@@ -1,19 +1,11 @@
 package jobsheet.akmal_dzaki;
 
-class Scincare{
-    String produk;
-    int harga;
+class motorr{
 
-    public Scincare(String produk, int harga){
-        this.produk = produk;
-        this.harga = harga;
-    }
 }
+
 public class js4 {
-  public static void main(String[] args) {
-    Scincare mahasiswa = new Scincare( "hanasui", 21);
-    
-    System.out.println("produk:" + mahasiswa.produk);
-    System.out.println("harga:" + mahasiswa.harga );
-  }  
+    public static void main(String[] args) {
+        motorr motor1 = new motorr();
+    }  
 }
