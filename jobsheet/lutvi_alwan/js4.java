34638b96630d7960d2aa8b59768effa0f05f8d2f
Lutfi Alwan <112716513+AlwanLutfi@users.noreply.github.com>
2023-10-25 19:50:40 +0800

    Update and rename mahasiswa.java to js4.java

---
+++ jobsheet/lutvi_alwan/js4.java
@@ -0,0 +1,30 @@
+public class mobil {
+    private String nama;
+    private int tahun;
+
+    public mobil() {
+        nama = "Avanza";
+        tahun = 2003;
+    }
+    public mobil(String nm, int tahun) {
+        this.tahun = tahun;
+        this.nama = nm;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public int getTahun() {
+        return tahun;
+    }
+
+    public static void main(String[] args) {
+        mobil mbl1 = new mobil("Xpander", 2012);
+        System.out.println("Jenis mobil: " + mbl1.getNama());
+        System.out.println("Tahun mobil: " + mbl1.getTahun());
+        System.out.println("");
+        System.out.println("");
+        mobil mbl2 = new mobil();
+        System.out.println("Jenis mobil: " + mbl2.nama);
+        System.out.println("Tahun mobil: " + mbl2.tahun);
+    }
+}
