0b1dbbb0cf1f8d4e25330bbf04d009211f6fff53
notforsilent <notforsilent@gmail.com>
2023-10-25 22:26:24 +0800

    terubah:         jobsheet/akmal_dzaki/js4.java

--- jobsheet/akmal_dzaki/js4.java
+++ jobsheet/akmal_dzaki/js4.java
@@ -1,17 +1,40 @@
 package jobsheet.akmal_dzaki;
 
+
 class manusia{
     String nama;
     int umur;
-    int bb;
+    int makan;
+    private double bb;
+        
 
     manusia(){
-
+        this.nama = nama;
+        this.umur = umur;
     }
+
+    public void setBb(double bb) {
+        bb = 51;
+        this.bb = bb+makan;
+    }
+    
+    public double getBb(){
+        return bb;
+    }
+   
 }
 
 public class js4 {
     public static void main(String[] args) {
+        // deklarasi objek orang
         manusia orang = new manusia();
+        orang.nama = "Jack";
+        orang.umur = 21;
+        orang.makan = 2;
+        orang.setBb(50);
+
+        System.out.println("\nNama saya: "+orang.nama);
+        System.out.println("Umur saya: "+orang.umur);
+        System.out.println("Berat badannya: "+orang.getBb());
     }  
 }
