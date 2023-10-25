50f0c905a48304e6cc53810eae8ffabab238a949
Lutfi Alwan <112716513+AlwanLutfi@users.noreply.github.com>
2023-10-25 19:42:21 +0800

    Update js1.java

--- jobsheet/lutvi_alwan/js1.java
+++ jobsheet/lutvi_alwan/js1.java
@@ -1,5 +1,11 @@
-package jobsheet.lutvi_alwan;
+public class mahasiswa {
+    int x = 9;
 
-public class js1 {
-    
+    public mahasiswa() {
+    }
+
+    public static void main(String[] args) {
+        mahasiswa mhs = new mahasiswa();
+        System.out.println(mhs.x);
+    }
 }
