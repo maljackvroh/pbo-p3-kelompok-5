e0899695fd18eb6ba4608b80cf1e0966a3578f79
notforsilent <notforsilent@gmail.com>
2023-10-27 17:58:21 +0800

    terubah:         studikasus.java

--- studikasus.java
+++ studikasus.java
@@ -6,6 +6,7 @@ class restoran{
     int bnyakMenu = 0;
     String menu[];
     String harga[];
+    boolean input;
 
     restoran(String namaResto, String alamatResto){
         this.namaResto = namaResto;
@@ -26,7 +27,7 @@ class restoran{
 
     public void getMenu(){
         System.out.println("Berikut daftar menu: ");
-        for(int i = 1; i < menu.length; i++){
+        for(int i = 1; i < bnyakMenu; i++){
             System.out.println(i+" "+menu[i-1]);
             System.out.println("Harga "+harga[i-1]);
         }
@@ -42,7 +43,7 @@ public class studikasus {
         Scanner in = new Scanner(System.in);
         restoran rstn = new restoran("Bensu", "Jln Sonokeling");
         
-        boolean input = true;
+        int input;
 
         do{
             System.out.println("Masukan nama menu: ");
@@ -51,18 +52,10 @@ public class studikasus {
             System.out.println("Masukan harga: ");
             rstn.setHarga(in.next());
 
-            System.out.println("Apakah anda ingin memasukan data lagi?(Y/N)");
-            String pil = in.next();
+            System.out.println("Apakah anda ingin memasukan data lagi?(1/0)");
+            rstn.input = in.nextBoolean();
 
-            if(pil == "Y"){
-                input = true;
-            } else if(pil == "N") {
-                input = false;
-            } else{
-                System.out.println("Inputan anda salah!");
-                input = false;
-            }
-        } while(input = true);
+        } while(rstn.input == true);
 
 
         rstn.getMenu();
